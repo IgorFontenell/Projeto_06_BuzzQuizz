942a4c4aa8b501933b33027b69045c568526a922
Pedro <driverique@gmail.com>
2022-04-22 13:57:04 -0300

    finaliznado parte de CriarPerguntas > Niveis

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ contadorDisplay = 0;
 
 
 
-
+telaCriaQuizz();
 
 
 // Aqui nós estamos trocando o layout dos quizzes do usuário vazio pelo com conteúdo e vice-versa
@@ -40,7 +40,7 @@ pegarQuizzes();
 function colocarQuizzes (response) {
 
     let quizzes = response.data;
-    console.log(quizzes);
+    
     for (let i = 0; i < quizzes.length; i++ ) {
         let procuracao = document.querySelector(".quizzesPublicos").querySelector("ul")
         procuracao.innerHTML = procuracao.innerHTML + `
@@ -64,22 +64,126 @@ function erroPegarMsg() {
 
 function criarQuizzSection(){
 
-    const telaPrincipal = document.querySelector('.container');
+    const telaPrincipal = document.querySelector('.quizzes');
+
     const criaQuizz = document.querySelector('.sectionCriarQuizz');
+
+    const quizzes = document.querySelector('.quizzes')
+
     criaQuizz.classList.remove('escondido');
     telaPrincipal.classList.add('escondido');
+    quizzes.classList.add('escondido');
 
 }
 
 function voltaTelaInicial(){
-    const telaPrincipal = document.querySelector('.container');
-    const criaQuizz = document.querySelector('.sectionCriarQuizz');
-    criaQuizz.classList.add('escondido');
-    telaPrincipal.classList.remove('escondido');
+   
+    location.reload();
+}
+
+function telaCriaQuizz(){
+
+    //ESSA FUNCAO ESTA RENDERIZANDO A PAGINA DE CRIACAO DE QUIZZ TELA 3 //
+    const sectionCriaQuizz = document.querySelector('.sectionCriarQuizz');
+
+    sectionCriaQuizz.innerHTML =`<div class="secao3-container">
+                
+                <h4>Comece pelo começo</h4>
+                <div class="input-blocks">
+                    <input class='inputQuizz' id="input1" type="text" maxlength="65" placeholder="Título do seu Quizz aqui">
+                    <input class='inputQuizz' id="input2" type="url" placeholder="URL do banner do seu quizz">
+                    <input class='inputQuizz' id="input3" type="number" placeholder="Quantidade de perguntas do quizz">
+                    <input class='inputQuizz' id="input4" type="number" placeholder="Quantidade de niveis do quizz">
+
+                    <button class="btn-criarquizz" onclick="telaCriaQuizz2()">Prosseguir para criar perguntas</button>
+                    <h7 onclick="voltaTelaInicial()">VOLTAR A TELA INICIAL QUIZZ</h7>
+                </div>
+   
+    </div>`
+
+}
+
+function telaCriaQuizz2(){
+
+    //ESSA FUNCAO ESTA RENDIRIZANDO A PROXIMA PAGINA DE CRIACAO COM OS VALORES DINAMICOS RECEBIDOS DO INPUT DA TELA ANTERIOR.
+    const inputTitulo = document.getElementById('input1').value;
+    const inputURL = document.getElementById('input2').value;
+    const inputQtdPerguntas = document.getElementById('input3').value;
+    const inputQtdNiveis = document.getElementById('input4').value;
+
+    let controle = 0;
+
+    if (inputTitulo.length < 20){
+        alert ("Por favor digite o titulo respeitando o minimo de caracteres - [20 CARACTERES NO MINIMO]");
+    } else controle += 1;
+    if (inputURL === ''){   
+        alert ("Por favor adcionar um URL VALIDA");
+    } else controle += 1;
+    if ((!Number.isInteger(inputQtdPerguntas)) && (inputQtdPerguntas < 3)){   
+        alert ("Entre com um valor válido para quantidade de Perguntas [NUMERO INTEIRO MAIOR QUE 3]");
+    } else controle += 1;
+    if (inputQtdNiveis <= 2) {   
+        alert ("Entre com a Quantidade de niveis válido [NUMERO INTEIRO MAIOR QUE 2]");
+    } else controle += 1;
+    
+    if (controle == 4){
+        telaCriarPerguntas(inputQtdPerguntas);
+    } 
+
 }
 
 
+function telaCriarPerguntas(qtdPerguntas){
+   
+    const sectionCriaQuizz = document.querySelector('.sectionCriarQuizz');
+    sectionCriaQuizz.classList.add('escondido');
 
+    const perguntas = document.querySelector('.sectionCriarQuizz2');
+    perguntas.classList.remove('escondido');
 
+    const botaoHabilitar = document.querySelector('.btn-criarquizz');
+    botaoHabilitar.classList.remove('escondido');
+
+    perguntas.innerHTML += `<h4>Crie as suas perguntas</h4>`
+
+    for(let i=0; i < qtdPerguntas; i++){
+
+            perguntas.innerHTML += `
+            <div class="secao3-container">
+                    
+                <div class="perguntasCreate">
+                    
+                    <div class="perguntaMinimizada">
+                        <h4>Pergunta ${i+1}</h4>
+                        <ion-icon name="create-outline"></ion-icon>
+                    </div>
+
+                    <section class="inputsPerguntas escondido">
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Texto da pergunta">
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Cor de fundo da pergunta">
+                        
+                        <h4>Resposta Correta</h4>
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Resposta Correta">
+                        <input class="inputCriacaoPerguntas" type="url" placeholder="URL da imagem">
+
+                        <h4>Respostas Incorretas</h4>
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Resposta Incorreta 1">
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="URL da imagem 1">
+                        
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Resposta Incorreta 2">
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="URL da imagem 2">
+                        
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="Resposta Incorreta 3">
+                        <input class="inputCriacaoPerguntas" type="text" placeholder="URL da imagem 3">
+                    </section>
+                       
+                </div>
+            </div>`
+    }
+
+        perguntas.innerHTML += `<button class="btn-criarquizz" onclick="telaCriaQuizz2()">Prosseguir para criar Níveis</button>
+        <h7 onclick="voltaTelaInicial()">VOLTAR A TELA INICIAL QUIZZ</h7>`
+        
+}
 
 
