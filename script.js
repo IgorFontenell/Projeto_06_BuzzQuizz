f551890070d1a104142f3ebe67d966b00e960e45
Igor <igorfontenelletexeira@gmail.com>
2022-04-22 21:52:29 -0300

    Versão com click no quizz quase funcionando

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let contadorDisplay = 0;
 
 contadorDisplay = 0;
 
+let arrayRespostas = [0, 1, 2, 3];
+
 
 
 
@@ -12,14 +14,14 @@ contadorDisplay = 0;
 function quizzesUsuarioLayout () {
     if (contadorDisplay >= 1) {
         let procuracao = document.querySelector(".quizzesVazio");
-        procuracao.classList.add("escondido");
+        procuracao.classList.add("escondido2");
         procuracao = document.querySelector(".quizzesOutroLayout");
-        procuracao.classList.remove("escondido");
+        procuracao.classList.remove("escondido2");
     } else if (contadorDisplay === 0) {
         let procuracao = document.querySelector(".quizzesVazio");
-        procuracao.classList.remove("escondido");
+        procuracao.classList.remove("escondido2");
         procuracao = document.querySelector(".quizzesOutroLayout");
-        procuracao.classList.add("escondido");
+        procuracao.classList.add("escondido2");
     }
 }
 quizzesUsuarioLayout();
@@ -43,14 +45,15 @@ function colocarQuizzes (response) {
     console.log(quizzes);
     for (let i = 0; i < quizzes.length; i++ ) {
         let procuracao = document.querySelector(".quizzesPublicos").querySelector("ul")
-        procuracao.innerHTML = procuracao.innerHTML + `
-                    <a href="index.html" target="_blanck">
+        procuracao.innerHTML = procuracao.innerHTML + 
+                            `
                             <li>
+                                <div class="quadradoClique" onclick="clicarQuizz(${quizzes[i].id})">
+                                </div>
                                 <div class="gradiente"></div>
-                                    <img src=${quizzes[i].image} alt="" width="340px" height="180px"/>
+                                    <img src=${quizzes[i].image} alt="Carregando imagem..." width="340px" height="180px"/>
                                     <span class="tituloQuizz">${quizzes[i].title}</span>
-                            </li>
-                    </a>`
+                            </li>`
 
     }
 
@@ -77,9 +80,133 @@ function voltaTelaInicial(){
     criaQuizz.classList.add('escondido');
     telaPrincipal.classList.remove('escondido');
 }
+// Funçao que ativa ao clicar no quizz e coloca aquele quizz clicado no html
+function clicarQuizz (element) {
+
+
+    let requisicao = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${element}`)
+    trocaDeEscondidos("container2");
+
+    requisicao.then(colocarNoDom);
+
+
+}
+
+function trocaDeEscondidos (classe) {
+    console.log("Funcionou");
+    let procuracao = document.querySelector(".escondido");
+    while (procuracao !== null) {
+        procuracao.classList.remove("escondido");
+        procuracao = document.querySelector(".escondido");
+        }
+    procuracao = document.querySelector(".container");
+    procuracao.classList.add("escondido");
+    procuracao = document.querySelector(".sectionCriarQuizz");
+    procuracao.classList.add("escondido");
+    procuracao = document.querySelector(".container2");
+    procuracao.classList.add("escondido");
+    
+    procuracao = document.querySelector(`.${classe}`);
+    procuracao.classList.remove("escondido");
+}
+
+function colocarNoDom (response) {
+    console.log(response.data);
+    let parametro = response.data
+    let perguntas = "";
+
+    let procura = document.querySelector(".container2");
+    procura.innerHTML = "" +
+    `<div class="banner"> 
+    <div class="gradientebanner">
+        <span>${parametro.title}</span>
+    </div>
+    <img class="bannerquizz" src="${parametro.image}" alt="Imagem carregando">
+</div>`;
+
+    for (let i = 0; i < parametro.questions.length; i++) {
+        let perguntaAtual = parametro.questions[i];
+        arrayRespostas = shuffleArray(arrayRespostas);
+        //Já colocamos o titulo geral do questionário agora escreveremos cada pergunta com as suas respostas aleatórias
+        perguntas += 
+        `<div class="pergunta1">
+        <div class="titulopergunta" style="background-color: ${perguntaAtual.color};">
+            <span>${perguntaAtual.title}</span>
+        </div>`
+        for (let i = 0; i < perguntaAtual.answers.length; i++) {
+            console.log(perguntaAtual.answers[arrayRespostas[i]].image);
+            perguntas += `
+            <div class="resposta">
+                <img src="${perguntaAtual.answers[arrayRespostas[i]].image}" alt="Imagem carregando" class="imgRespostas">
+                <h6>${perguntaAtual.answers[arrayRespostas[i]].text}</h6>
+            </div>`
+        }
+         
+        perguntas += "</div>";
+    
+    }
+    procura.innerHTML += `${perguntas}`;
+    
 
 
 
 
 
 
+
+
+
+}
+
+function shuffleArray(inputArray){
+   return inputArray.sort(()=> Math.random() - 0.5);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let verDepois = `<div class="pergunta1"> 
+    <div class="titulopergunta">
+        <span>Em qual animal Olho-Tonto Moody transfigurou Malfoy?</span>
+    </div>
+    
+    <div class="respostas">
+        <div class="resposta">
+            <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/01/ford-mustang-gt350r-16.jpg?resize=650,434" alt="mustangao nervoso" class="imgRespostas">
+            <h6>Mustangao brabo</h6>
+        </div>
+        <div class="resposta">
+            <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/01/ford-mustang-gt350r-16.jpg?resize=650,434" alt="mustangao nervoso" class="imgRespostas">
+            <h6>Mustangao brabo</h6>
+        </div>
+       
+    </div>
+    <div class="respostas">
+        <div class="resposta">
+            <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/01/ford-mustang-gt350r-16.jpg?resize=650,434" alt="mustangao nervoso" class="imgRespostas">
+            <h6>Mustangao brabo</h6>
+        </div>
+        <div class="resposta">
+            <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/01/ford-mustang-gt350r-16.jpg?resize=650,434" alt="mustangao nervoso" class="imgRespostas">
+            <h6>Mustangao brabo</h6>
+        </div>
+    </div>
+</div>`;
