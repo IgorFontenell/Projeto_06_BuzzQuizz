47fbc533ea36665a33931851b0c1cb3fc77742ff
Pedro <driverique@gmail.com>
2022-04-25 23:01:39 -0300

    niveis ok, falta location

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const URL_QUIZZ = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
 let expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 let regex = new RegExp(expression);
 let contadorDisplay = 0;
-
+let qtdNiveis = 0;
 contadorDisplay = 0;
 
 let arrayRespostas = [0, 1, 2, 3];
@@ -82,8 +82,10 @@ function criarQuizzSection(){
 }
 
 function voltaTelaInicial(){
-   
-    location.reload();
+    if(confirm('Tem certeza que deseja cancelar e voltar à pagina inicial do BUZZQUIZZ?')){
+        location.reload();
+    }
+    
 }
 // Funçao que ativa ao clicar no quizz e coloca aquele quizz clicado no html
 function clicarQuizz (element) {
@@ -220,6 +222,7 @@ function telaCriaQuizz2(){
         telaCriarPerguntas(inputQtdPerguntas);
     } 
 
+    return qtdNiveis = inputQtdNiveis;
 }
 
 
@@ -268,16 +271,127 @@ function telaCriarPerguntas(qtdPerguntas){
             </div>`
     }
 
-        perguntas.innerHTML += `<button class="btn-criarquizz" onclick="telaCriaQuizz2()">Prosseguir para criar Níveis</button>
+        perguntas.innerHTML += `<button class="btn-criarquizz" onclick="decidindoNiveis()">Prosseguir para criar Níveis</button>
         <h7 onclick="voltaTelaInicial()">VOLTAR A TELA INICIAL QUIZZ</h7>`
         
 }
+
+function decidindoNiveis(){
+    // AQUI ESTAMOS INDO PARA A PAGINA DE DEFINICAO DOS NIVEIS.
+     
+     console.log(qtdNiveis);
+     const sectionCriaQuizz = document.querySelector('.sectionCriarQuizz');
+     sectionCriaQuizz.innerHTML = ' ';
+     sectionCriaQuizz.classList.remove('escondido');
+ 
+     const perguntas = document.querySelector('.sectionCriarQuizz2');
+     perguntas.classList.add('escondido');
+ 
+     sectionCriaQuizz.innerHTML += `<h4>Agora, decida os seus níveis!</h4>`
+      
+     for(let i=0; i < qtdNiveis; i++){
+ 
+        sectionCriaQuizz.innerHTML += `
+             <div class="secao3-container">
+                     
+                 <div class="perguntasCreate">
+                     
+                     <div class="perguntaMinimizada" id="${i+1}">
+                         <h4>Nivel ${i+1}</h4>
+                         <ion-icon onclick='expandirPergunta(${i+1})' name="create-outline"></ion-icon>
+                     </div>
+ 
+                     <section class="inputsPerguntas escondido">
+                         <input class="inputCriacaoPerguntas" id="tituloNivel${i+1}" type="text" placeholder="Titulo do nivel">
+                         <input class="inputCriacaoPerguntas" type="number" id="acertoMin${i+1}" placeholder="% de acerto minima">
+                         <input class="inputCriacaoPerguntas" type="text" id="urlNivel${i+1}" placeholder="URL da imagem do nível">
+                         <input class="inputDescription" type="text" id="descricaoNivel${i+1}" placeholder="Descrição do nível">
+                        
+                     </section>
+                        
+                 </div>
+             </div>`
+     }
+ 
+     sectionCriaQuizz.innerHTML += `<button class="btn-criarquizz" onclick="enviarQuizz()">Finalizar envio do QUIZ</button>
+     <h7 onclick="voltaTelaInicial()">CANCELAR ENVIO DO QUIZZ</h7>`
+         
+ }
+
+ function enviarQuizz(){
+
+    let contador = 0;
+    let controleAcertoMin = false;
+
+    for(let i=0; i < qtdNiveis; i++){
+    
+        const tituloNivel = document.getElementById(`tituloNivel${i+1}`).value;
+        const acertoMin = document.getElementById(`acertoMin${i+1}`).value;
+        const urlNivel = document.getElementById(`urlNivel${i+1}`).value;
+        const descricaoNivel = document.getElementById(`descricaoNivel${i+1}`).value;
+       
+    
+        if (tituloNivel.length < 10){
+            alert(`VOCÊ ESTA COM PROBLEMAS NO NIVEL > ${i+1} \n \nPor favor o titulo do nivel tem que ter no minimo 10 caracteres...`);
+        } else contador ++; 
+        if ((!Number.isInteger(acertoMin)) && (acertoMin > 100)){
+           
+            alert(`VOCÊ ESTA COM PROBLEMAS NO NIVEL > ${i+1} \n \n
+            Por favor insira os dados corretamente [ 0 A 100 ]`);
+
+        } else if (acertoMin == 0){
+            controleAcertoMin = true;
+            contador ++
+        } else contador ++;
+        if (!urlNivel.match(regex)){   
+            alert(`VOCÊ ESTA COM PROBLEMAS NO NIVEL > ${i+1} \n\n
+            URL da Imagem: deve ter formato de URL`);
+        } else contador ++;
+        if (descricaoNivel.length < 30){
+            alert(`VOCÊ ESTA COM PROBLEMAS NO NIVEL > ${i+1} \n \n
+            Por favor a descrição precisa ter no minimo 30 caracteres preencha de maneira correta.`);
+        }else contador ++;
+        
+       
+    } // final do for
+   
+    if ((contador == qtdNiveis*4) && (controleAcertoMin == true)){ //verificando se existe pelo menos 1 nivel que tenha o 0%;
+        console.log('Conseguiu com sucesso');
+        quizzFinalizado(contador);
+    } else {
+        alert('Por favor você precisa colocar pelo menos um nivel com o valor de Acerto minimo 0%.');
+    }
+
+}// fim da funcao envia quizz
+
+function quizzFinalizado(){
+    const sectionCriaQuizz = document.querySelector('.sectionCriarQuizz');
+    sectionCriaQuizz.classList.add('escondido');
+
+    const perguntas = document.querySelector('.sectionCriarQuizz2');
+    perguntas.innerHTML = '';
+    perguntas.classList.remove('escondido');
+
+    perguntas.innerHTML = `<h7> PARABENS VC CONSEGUIU COM SUCESSO BOLODINHO.`
+
+    
+
+
+}
+
 
 function expandirPergunta(id){
     //aqui estamos expandindo as perguntas
     const iconeClicado = document.getElementById(id).nextElementSibling;
     iconeClicado.classList.toggle('escondido');
 }
+
+
+
+
+
+
+
 
 // Isso aqui é só pra caso dê algum bug a gente comparar com o html original
 let verDepois = `<div class="pergunta1"> 
